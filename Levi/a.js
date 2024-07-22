e8c20f96b3725c2a9f6f26b3628767c41a69c65c
Levi Goold <levi.goold@gmail.com>
2024-07-22 14:18:41 -0700

    Added flask

---
+++ Levi/a.js
@@ -0,0 +1,7 @@
+
+
+d3.json("http://127.0.0.1:5000/api/v1.0/hospitals").then((data) => {
+
+  console.log(data);
+
+});
---
+++ Levi/flask_api.py
@@ -0,0 +1,53 @@
+import sqlite3
+from flask import Flask
+
+app = Flask(__name__)
+
+def get_table(table_name):
+  conn = sqlite3.connect('data.sqlite')
+  conn.row_factory = sqlite3.Row
+  db = conn.cursor()
+
+  rows = db.execute(f"SELECT * FROM {table_name}").fetchall()
+
+  conn.commit()
+  conn.close()
+
+  return [dict(row) for row in rows]
+
+def link(url):
+  return f"<a href={url}>{url}</a><br>"
+
+@app.route("/")
+def home():
+  return (
+    "Available Routes:<br>" +
+    link("/api/v1.0/hospitals") +
+    link("/api/v1.0/military_bases") + 
+    link("/api/v1.0/us_population") + 
+    link("/api/v1.0/gun_sales") + 
+    link("/api/v1.0/covid_cases")
+  )
+
+@app.route("/api/v1.0/hospitals")
+def hospitals():
+  return get_table("hospitals")
+
+@app.route("/api/v1.0/military_bases")
+def military_bases():
+  return get_table("military_bases")
+
+@app.route("/api/v1.0/us_population")
+def us_population():
+  return get_table("us_population")
+
+@app.route("/api/v1.0/gun_sales")
+def gun_sales():
+  return get_table("gun_sales")
+
+@app.route("/api/v1.0/covid_cases")
+def covid_cases():
+  return get_table("covid_cases")
+
+if __name__ == '__main__':
+  app.run(debug=True)
